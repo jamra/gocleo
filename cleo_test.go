e39926b5c0c7a04125f05f8bbc87912964671672
jamra <jamra.source@gmail.com>
2013-12-25 19:12:54 -0800

    I committed the wrong test last time
    Here is the right test file.

--- cleo_test.go
+++ cleo_test.go
@@ -1,25 +1,17 @@
-//package cleo_test
+package cleo
 
-//import "testing"
-//import "cleo"
+import "testing"
 
-////func TestScore(t *testing.T) {
-////if Score("abcdefghij", "abcdefghix") != 0.90 {
-////	t.Fail()
-////}
-////}
+func TestLevenshtein(t *testing.T) {
+	if LevenshteinDistance("abcdefghij", "abcdefghix") != 1 {
+		t.Fail()
+	}
 
-////func TestLevenshtein(t *testing.T) {
-////if LevenshteinDistance("abcdefghij", "abcdefghix") != 1 {
-////	t.Fail()
-////}
+	if LevenshteinDistance("abcdefghij", "abcdefghijk") != 1 {
+		t.Fail()
+	}
 
-////if LevenshteinDistance("abcdefghij", "abcdefghijk") != 1 {
-////	t.Fail()
-////}
-
-////if LevenshteinDistance("abcdefghij", "abcdefghi") != 1 {
-////	t.Fail()
-////}
-
-////}
+	if LevenshteinDistance("abcdefghij", "abcdefghi") != 1 {
+		t.Fail()
+	}
+}
