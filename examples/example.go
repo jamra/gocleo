170ad9c44d716d196ec319f7ba47489398793da1
jamra <jacob.amrany@gmail.com>
2013-12-26 11:38:19 -0800

    Updated the example
    Now the user needs to implement his own http handler. 
    
    The url will be localhost:9999/cleo?query=wolf

--- examples/example.go
+++ examples/example.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/jamra/gocleo"
+import (
+	"github.com/jamra/gocleo"
+	"net/http"
+)
 
 func main() {
-	cleo.InitAndRun("./w1_fixed.txt", "8080", nil)
+	cleo.BuildIndexes("./w1_fixed.txt", nil)
+	err := http.ListenAndServe(":9999", nil)
+	if err != nil {
+		panic(err)
+	}
 }
